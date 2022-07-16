8d1f6c532067c68d072ab2158a2759c7b3ef4b3d
Cento <tanhiep26@gmail.com>
2022-07-16 21:50:49 +0700

    Hello World!

---
+++ HelloWorld/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!!</Text>
+      <Text style={styles.red}>A big red Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  red: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
+});
